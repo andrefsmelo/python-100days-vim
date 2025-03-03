d6162c0d746a1763fd8d4213f2ae967c81dd1d5a
andre <andreemelo@gmail.com>
2025-03-03 22:12:23 +0000

    Day3 - Control Flow

---
+++ day-01to15/day3_project.py
@@ -0,0 +1,16 @@
+print('''
+888                                                          
+888                                                          
+888                                                          
+888888888d888 .d88b.  8888b. .d8888b 888  888888d888 .d88b.  
+888   888P"  d8P  Y8b    "88b88K     888  888888P"  d8P  Y8b 
+888   888    88888888.d888888"Y8888b.888  888888    88888888 
+Y88b. 888    Y8b.    888  888     X88Y88b 888888    Y8b.     
+ "Y888888     "Y8888 "Y888888 88888P' "Y88888888     "Y8888  
+''')
+print("Welcome to Treasure Island.")
+print("You mission is to find a treasure.")
+choice1 = input('You\'re at a crossroad, where do you want to go? '
+                'Type "Left" or "right".').lower()
+
+
