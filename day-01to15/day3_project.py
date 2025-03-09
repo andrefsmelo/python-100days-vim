d42cae7201edafffcc351ccaea9adc5c8660d389
andre <andreemelo@gmail.com>
2025-03-09 20:16:46 +0000

    Day 3 - Project

--- day-01to15/day3_project.py
+++ day-01to15/day3_project.py
@@ -11,6 +11,28 @@ Y88b. 888    Y8b.    888  888     X88Y88b 888888    Y8b.
 print("Welcome to Treasure Island.")
 print("You mission is to find a treasure.")
 choice1 = input('You\'re at a crossroad, where do you want to go? '
-                'Type "Left" or "right".').lower()
+                'Type "left" or "right".\n').lower()
 
-
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. '
+                   'There is an island in the middle of the lake. '
+                   'Type "wait" to wait for the boat. '
+                   'Type "swim" to swim across.\n').lower()
+    
+    if choice2 == "wait":
+        choice3 = input('You arrived at the island unharmed. '
+                        'There is a houde with 3 doors. One red, '
+                        'one yellow and one blue. '
+                        'Which colour do you chose?\n').lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure. You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")     
+    else:
+        print("You got attacked by an angry trout. Game Over")
+else:
+    print("You fell in to a hole. Game Over.")
